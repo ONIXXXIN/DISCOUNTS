50d4cd2c88041a73f46f05a5bf36edb4a7a8a60a
vladt <vladtihonov1183@gmail.com>
2025-05-16 20:03:31 +0200

    ю

--- 😎.py
+++ 😎.py
@@ -1,27 +1,73 @@
 import sys
-
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit
 
-
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Bababoui")
+        self.setGeometry(1200, 300, 500, 600)
+        self.setStyleSheet("background-color:#000000")
 
-        button = QPushButton("Press Me!")
+        # Кнопка
+        self.lolikhopter = QPushButton("nolik", self)
+        self.lolikhopter.setGeometry(130, 200, 300, 100)  # Уменьшил размеры, чтобы оставить место
+        self.lolikhopter.clicked.connect(self.score)
+        self.lolikhopter.setStyleSheet("""
+        QPushButton {
+            font-size: 40px;
+            background-color: #f8fc00;
+            color: black;
+            border-radius: 25% 
+        }
+        QPushButton:hover {
+            font-size: 40px;
+            background-color: #d6d918;
+            color: black;
+            border-radius: 35% 
+        }
+        QPushButton:pressed {
+            font-size: 50px;
+            background-color: #aba732;
+            color: black;
+            border-radius: 40% 
+        }
+        """)
 
-        self.setFixedSize(QSize(400, 300))
+        # Метка с текстом
+        self.label = QLabel("Введите текст:", self)
+        self.label.setGeometry(50, 50, 200, 30)
+        self.label.setStyleSheet("color: white; font-size: 16px;")
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        # Поле ввода
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(200, 50, 200, 30)
+        self.input_field.setStyleSheet("""
+                QLineEdit {
+                    font-size: 40px;
+                    background-color: #f8fc00;
+                    color: black;
+                    border-radius: 25%;
+                }
+                QLineEdit:hover {
+                    font-size: 40px;
+                    background-color: #d6d918;
+                    color: black;
+                    border-radius: 35%;
+                }
+                QLineEdit:focus {
+                    font-size: 50px;
+                    background-color: #aba732;
+                    color: black;
+                    border-radius: 40px;
+                }
+                """)
 
+    def score(self):
+        print("no")
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec()
